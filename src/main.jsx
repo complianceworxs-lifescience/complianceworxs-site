3cc4b7d4b61d6a85f43aeede0d9d6cae4030a1a0
ComplianceWorxs <complianceworxs@gmail.com>
2026-02-08 12:38:29 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App.jsx";
 import Pricing from "./Pricing.jsx";
 import Intelligence from "./Intelligence.jsx";
+import Authority from "./Authority.jsx";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -13,6 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/" element={<App />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/intelligence" element={<Intelligence />} />
+        <Route path="/authority" element={<Authority />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
