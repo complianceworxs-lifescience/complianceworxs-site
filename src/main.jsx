cfacb4a69c6bf737b221c7e130637cfd541bdaf5
ComplianceWorxs <complianceworxs@gmail.com>
2026-02-09 19:19:50 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-import React from "react"; 
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import App from "./App.jsx";
 import Pricing from "./Pricing.jsx";
 import Intelligence from "./Intelligence.jsx";
 import Authority from "./Authority.jsx";
-import SecureAccess from "./SecureAccess.jsx"; // Ensure this import is correct
-import DDR from "./DDR.jsx"; // Import the DDR component
-
+import SecureAccess from "./SecureAccess.jsx";
+import DDR from "./DDR.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -20,7 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/intelligence" element={<Intelligence />} />
         <Route path="/authority" element={<Authority />} />
         <Route path="/secure-access" element={<SecureAccess />} />
-        <Route path="/ddr" element={<DDR />} /> {/* DDR Route added */}
+        <Route path="/ddr" element={<DDR />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
