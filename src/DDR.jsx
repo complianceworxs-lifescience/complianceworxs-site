4c427e6f8f57ed3c751de9d9eae99a906f8dd47a
ComplianceWorxs <complianceworxs@gmail.com>
2026-02-10 15:21:44 -0500

    Create DDR.jsx

---
+++ src/DDR.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DDR = () => {
+  return (
+    <div className="ddr-container">
+      <h1>Welcome to the DDR Page</h1>
+      <p>This is where the new DDR component will live.</p>
+    </div>
+  );
+};
+
+export default DDR;
