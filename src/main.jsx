64d2afe5e38d93bea1c57b34f92d6defad1a7cef
ComplianceWorxs <complianceworxs@gmail.com>
2026-02-12 17:27:25 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 
-// POINTING TO LOWERCASE FILES IN THE SRC FOLDER
+// FIXED: Now pointing to Assessment.jsx (which you just renamed)
 import DDRLayout from './DDRLayout';
-import Assessment from './assessment'; 
+import Assessment from './Assessment'; 
 import Context from './ddr/Context'; 
 import Evidence from './ddr/Evidence';
 import Risk from './ddr/Risk';
