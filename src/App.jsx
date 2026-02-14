d50079954001150a4349cfd972c7e48154aa2722
ComplianceWorxs <complianceworxs@gmail.com>
2026-02-14 06:15:11 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, Outlet, Navigate } from 'react-router-dom';
++ import { HashRouter as Router, Routes, Route, Link, Outlet, Navigate } from 'react-router-dom';
 
 // Engine Imports
 import DDRLayout from "./DDRLayout";
