19b996672adef562f2efbf673c0c60a31ce6e3e0
ComplianceWorxs <complianceworxs@gmail.com>
2026-02-18 06:40:26 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -220,7 +220,7 @@ const Home = () => {
           backgroundImage: 'url("/images/Screenshot (15).png")',
           backgroundSize: 'cover',
           backgroundPosition: 'center',
-          opacity: .3,
+          opacity: .8,
           zIndex: 0
         }} />
 
