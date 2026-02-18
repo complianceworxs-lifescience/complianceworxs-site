dc1fdcd8c9954e8925ecee7b36c598e35c5dfeb4
ComplianceWorxs <complianceworxs@gmail.com>
2026-02-18 09:49:01 -0500

    Update IntelligenceStream.jsx

--- src/pages/IntelligenceStream.jsx
+++ src/pages/IntelligenceStream.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import MainLayout from "./App";
+import MainLayout from "../App";
 
 export default function IntelligenceStream() {
   const navigate = useNavigate();
